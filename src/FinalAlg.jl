a0ecc2cee80e59638b9694f84a9cddb04e3195ce
Gabriel Bianek <gabrielbianek151@gmail.com>
2024-04-29 09:12:47 -0300

    "V12 - 29-04 - 09:12"

--- src/FinalAlg.jl
+++ src/FinalAlg.jl
@@ -29,16 +29,16 @@ function RESULTS(Init::Dict; TABLE = "OFF")
     MAIN_RESULTS["w_net"] = MAIN_RESULTS["w_out"] - MAIN_RESULTS["w_in"]
     MAIN_RESULTS["η_t"] = MAIN_RESULTS["w_net"]/MAIN_RESULTS["q_in"]
     MAIN_RESULTS["rct"] = MAIN_RESULTS["w_in"]/MAIN_RESULTS["w_out"]
+    df = DataFrame(PARAMS = ["Eficiência térmica de primeira Lei", "Calor que entra", "Trabalho líquido"], B = [MAIN_RESULTS["η_t"]*100, MAIN_RESULTS["q_in"], MAIN_RESULTS["w_net"]])
+    if Init["INPUT"]["MODELO"] != "FTHA" && Init["INPUT"]["Δt_c"] == 0
+        rename!(df, :B => "FTHA")
+    else
+        rename!(df, :B => Init["INPUT"]["MODELO"])
+    end
     if TABLE == "OFF"
         return MAIN_RESULTS
     elseif TABLE == "ON"
-        df = DataFrame(PARAMS = ["Eficiência térmica de primeira Lei", "Calor que entra", "Trabalho líquido"], B = [MAIN_RESULTS["η_t"]*100, MAIN_RESULTS["q_in"], MAIN_RESULTS["w_net"]])
-        if Init["INPUT"]["MODELO"] != "FTHA" && Init["INPUT"]["Δt_c"] == 0
-            rename!(df, :B => "FTHA")
-        else
-            rename!(df, :B => Init["INPUT"]["MODELO"])
         return pretty_table(df, show_row_number = false)
-        end
     end
 end
 
