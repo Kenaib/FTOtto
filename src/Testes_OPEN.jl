2cb1fd35084e00e459699b29714ee8fb8f4112ad
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-08-30 15:01:27 -0300

    DEL case studies under src/

--- src/Testes_OPEN.jl
+++
@@ -1,9 +0,0 @@
-using .FTOtto
-using Plots
-
-X = FTOtto.Init_Parameters(r_compr = 14, MODELOS = "FTAF", Fluido = "C4H10", ϕ = 0.01, Half_lifes = 10, Y_FRAC = "iK", α = 0.05, q_ent = 0)
-Y = FTOtto.Initialization(X)
-FTOtto.cFTAFik(Y)
-RES = FTOtto.RESULTS(Y)
-FTOtto.TABLES(RES)
-Y["INPUT"]["FLUID"]["m_R"]
