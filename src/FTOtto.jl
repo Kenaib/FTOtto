bd0d793b2ac593622619a03ef516aca47a2d0b04
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-08-30 16:09:51 -0300

    UPD the src/FTOtto.jl

--- src/FTOtto.jl
+++ src/FTOtto.jl
@@ -2,22 +2,9 @@ module FTOtto
 
 ###Bibliotecas
 using IdealGasLib
-using DrWatson
 using Plots
 using DataFrames
 using PrettyTables
-
-###Setup DrWatson.jl
-initialize_project("FTOtto_SIMULATION"; authors="GB", force = true)
-
-function SAVE_SIM(;Init_Data::Dict, Results::Dict, Name = nothing)
-    safesave(datadir("SIMULATION_INITIAL_DATA", savename(Name, "jld2")), Init_Data)
-    safesave(datadir("SIMULATION_RESULTS", savename("R_", Name, "jld2")), Results)
-end
-
-function SAVE_PLOTS(;Plot_name::AbstractString, Plot)
-    safesave(plotsdir("FTOtto_Plots", Plot_name*".png"), Plot)
-end
 
 ###Função de dados iniciais 
 
