37ef4434a9aa7827ea8cbc80fd7d8fe598aae1af
Gabriel Bianek <gabrielbianek151@gmail.com>
2024-09-02 13:53:03 -0300

    IK Improvements

--- src/cFTAF-iK.jl
+++ src/cFTAF-iK.jl
@@ -13,7 +13,7 @@ function cFTAFik(Init::Dict)
             if abs(Init["SIMUL"]["α"][i] - Init["INPUT"]["θ"]) >= (Init["TOL"]["ϵ_v"])^(1/2) || Init["INPUT"]["Δt_c"] != nothing
                 push!(y_iii, y_alpha(Init, Init["SIMUL"]["α"][i]))
             elseif abs(Init["SIMUL"]["α"][i] - Init["INPUT"]["θ"]) <= (Init["TOL"]["ϵ_v"])^(1/2)
-                Init["INPUT"]["Δt_c"] = chem_time(Init, i)
+                Init["INPUT"]["Δt_c"] = chem_time(Init, i, CombTimeIK = "OFF")
                 Init["INPUT"]["δ"] = Init["INPUT"]["ω"] * Init["INPUT"]["Δt_c"]
                 push!(y_iii, y_alpha(Init, Init["SIMUL"]["α"][i]))
             end
@@ -37,7 +37,7 @@ function cFTAFik(Init::Dict)
             if abs(Init["SIMUL"]["α"][i] - Init["INPUT"]["θ"]) >= (Init["TOL"]["ϵ_v"])^(1/2) || Init["INPUT"]["Δt_c"] != nothing
                 push!(y_iii, y_alpha(Init, Init["SIMUL"]["α"][i]))
             elseif abs(Init["SIMUL"]["α"][i] - Init["INPUT"]["θ"]) <= (Init["TOL"]["ϵ_v"])^(1/2)
-                Init["INPUT"]["Δt_c"] = chem_time(Init, i)
+                Init["INPUT"]["Δt_c"] = chem_time(Init, i, CombTimeIK = "OFF")
                 Init["INPUT"]["δ"] = Init["INPUT"]["ω"] * Init["INPUT"]["Δt_c"]
                 push!(y_iii, y_alpha(Init, Init["SIMUL"]["α"][i]))
             end
@@ -67,9 +67,9 @@ function cFTAFik(Init::Dict)
         
     end
 
-    while length(Init["SIMUL"]["𝔽_ik"]) < length(Init["SIMUL"]["α"])
-        push!(Init["SIMUL"]["𝔽_ik"], Init["SIMUL"]["𝔽_ik"][end])
-    end
+    #while length(Init["SIMUL"]["𝔽_ik"]) < length(Init["SIMUL"]["α"])
+    #    push!(Init["SIMUL"]["𝔽_ik"], Init["SIMUL"]["𝔽_ik"][end])
+    #end
     
 end
 
